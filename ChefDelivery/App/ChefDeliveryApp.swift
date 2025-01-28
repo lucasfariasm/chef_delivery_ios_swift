ffacb841a4e1bae7eb0bc20f7b0e57a45ca543cc
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-01-28 17:08:34 -0300

    add animations

--- ChefDelivery/App/ChefDeliveryApp.swift
+++ ChefDelivery/App/ChefDeliveryApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ChefDeliveryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -1,19 +1,33 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State private var currentIndex = 1
+    
     let ordersMock: [OrderType] = [
         OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
         OrderType(id: 2, name: "banner prato feito", image: "brazilian-meal-banner"),
         OrderType(id: 3, name: "banner poke", image: "pokes-banner"),
     ]
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersMock){ mock in
                 CarouselItemView(order: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear{
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
+                _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
+        }
     }
 }
 
